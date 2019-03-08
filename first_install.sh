a66ea830565c5a2214e3e0acc3488e939e0cc93c
azrael2162 <vincent.pinelli2162@gmail.com>
2019-03-08 10:40:56 +0100

    Update first_install.sh

--- first_install.sh
+++ first_install.sh
@@ -5,20 +5,20 @@ reset
 
 apt upgrade && apt update
 #--------------------------------------
-apt install apache2 
-apt install php7.0
-apt install libapache2-mod-php7.0
-apt install fail2ban
-apt install portsentry
-apt install mysql-server
-apt install curl
-apt install iptables
-apt install ufw 
+apt install apache2 -y
+apt install php7.0 -y
+apt install libapache2-mod-php7.0 -y
+apt install fail2ban -y 
+apt install portsentry -y
+apt install mysql-server -y 
+apt install curl -y 
+#apt install iptables 
+#apt install ufw 
 apt autoremove
 #----------------------------------
 #lanch service
 service fail2ban start
-service portsentry start
+#service portsentry start
 service apache2 restart
 service --status-all
 #-----------------------------------
@@ -29,15 +29,15 @@ echo "config port iptable"
 sleep 1
 
 
-ufw allow 80
-ufw allow 22 
-ufw allow 21
-ufw allow 446
-ufw allow 443
-ufw allow 25
-ufw reject 23
-ufw enable
-ufw status
+#ufw allow 80
+#ufw allow 22 
+#ufw allow 21
+#ufw allow 446
+#ufw allow 443
+#ufw allow 25
+#ufw reject 23
+#ufw enable
+#ufw status
 
 sleep 2 
 reset
